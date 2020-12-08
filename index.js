bdf946efcdc8818c585815e9ad83c89d3f5f9220
Raha Moosavi <rahamoosavi1378@gmail.com>
2020-12-08 17:21:26 +0100

    bug developer start

---
+++ index.js
@@ -0,0 +1,79 @@
+var TxtRotate = function (el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = "";
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function () {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+
+    var that = this;
+    var delta = 300 - Math.random() * 100;
+
+    if (this.isDeleting) {
+        delta /= 2;
+    }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === "") {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    setTimeout(function () {
+        that.tick();
+    }, delta);
+};
+
+window.onload = function () {
+    var elements = document.getElementsByClassName("txt-rotate");
+    for (var i = 0; i < elements.length; i++) {
+        var toRotate = elements[i].getAttribute("data-rotate");
+        var period = elements[i].getAttribute("data-period");
+        if (toRotate) {
+            new TxtRotate(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+    document.body.appendChild(css);
+};
+
+let hidden = document.querySelectorAll('.hidden');
+setTimeout(()=>{
+    for (let i = 0; i < hidden.length; i++) {
+        hidden[i].classList.remove('hidden');
+    };
+},13000);    
+    
+let clickMe = document.getElementById("top2");
+clickMe.addEventListener("click", () => {
+    document.getElementById("top").style.top = "-100vh";
+    document.getElementById("down").style.top = "-100vh";
+
+});
+let video = document.getElementById("video");
+video.addEventListener("click", () => {
+    setTimeout(() => {
+        document.getElementById("top").style.top = 0;
+        document.getElementById("down").style.top = 0;
+    }, 18000);
+});
